67a4c290c0f86f1b32e97cc87da1e9220852ca22
Dan Osborne <dan.osborne@rocketmail.com>
2017-04-24 15:26:04 +0100

    Refactor var names

--- Assets/Scripts/Hero.js
+++ Assets/Scripts/Hero.js
@@ -3,9 +3,9 @@
 public var moveSpeed = 55;
 
 function Update () {
-	var X = Input.GetAxis("Horizontal");
-	var Y = Input.GetAxis("Vertical");
-	var Xamt = moveSpeed*X*Time.deltaTime;
-	var Yamt = moveSpeed*Y*Time.deltaTime;
-	transform.Translate(Xamt,Yamt,0);
+	var Hor = Input.GetAxis("Horizontal");
+	var Ver = Input.GetAxis("Vertical");
+	var Hamt = moveSpeed*Hor*Time.deltaTime;
+	var Vamt = moveSpeed*Ver*Time.deltaTime;
+	transform.Translate(Hamt,Vamt,0);
 }
