dd5bde85a6a79ca20ecc38bd2181b0e1701128a0
Dan Osborne <dan.osborne@rocketmail.com>
2017-04-25 16:19:11 +0100

    Added shooting logic

--- Assets/Scripts/Hero.js
+++ Assets/Scripts/Hero.js
@@ -1,33 +1,59 @@
 ﻿#pragma strict
 
-var moveSpeed = 16;
+var moveSpeed: float = 16;
+var fireRate: float = 0.5;
+var bullet: GameObject;
+var bulletThrust: float = 100;
+var distance = 10.0;
+
+private var nextFire: float = 0.5;
+private var newBullet: GameObject;
+private var myTime: float = 0.0;
 
 function start() {
 	// print("hello");
 }
 
+function Update() {
+	handleMovement();
+
+	if (Input.GetMouseButton(0)) {
+		handlePrimary();
+	}
+
+	if (Input.GetMouseButton(1)) {
+		handleSecondary();
+	}
+}
+
+// Moves the player
 function handleMovement() {
 	var Hor = Input.GetAxis("Horizontal");
 	var Ver = Input.GetAxis("Vertical");
-	var Hamt = moveSpeed*Hor*Time.deltaTime;
-	var Vamt = moveSpeed*Ver*Time.deltaTime;
-	transform.Translate(Hamt,Vamt,0);
+	var Hamt = moveSpeed * Hor * Time.deltaTime;
+	var Vamt = moveSpeed * Ver * Time.deltaTime;
+	transform.Translate(Hamt, Vamt, 0);
 }
 
+// Shoot primary weapon
 function handlePrimary() {
-	if(Input.GetMouseButton(0)){
-		print("primary weapon used");
-	}
+	print("Primary weapon used");
+
+	nextFire = myTime + fireRate;
+	newBullet = Instantiate(bullet, transform.position, transform.rotation);
+
+	// create code here that animates the newProjectile
+
+		//  var position = Vector3(Input.mousePosition.x, Input.mousePosition.y, distance);
+        //  position = Camera.main.ScreenToWorldPoint(position);
+        //  newBullet.transform.LookAt(position);    
+        //  Debug.Log(position);    
+        //  newBullet.GetComponent.<Rigidbody2D>().AddForce(newBullet.transform.forward * bulletThrust);
+
+	nextFire = nextFire - myTime;
+	myTime = 0.0;
 }
 
 function handleSecondary() {
-	if(Input.GetMouseButton(1)){
-		print("Secondary weapon used");
-	}
-}
-
-function Update() {
-	handleMovement();
-	handlePrimary();
-	handleSecondary();
+	print("Secondary weapon used");
 }
