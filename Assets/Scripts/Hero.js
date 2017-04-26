97c8009b3ea1bf99b098412a0922576323c888ab
Dan Osborne <dan.osborne@rocketmail.com>
2017-04-26 15:22:19 +0100

    Removed un-used crap

--- Assets/Scripts/Hero.js
+++ Assets/Scripts/Hero.js
@@ -3,16 +3,7 @@
 var moveSpeed: float = 16;
 var fireRate: float = 0.5;
 var bullet: GameObject;
-var bulletThrust: float = 100;
-var distance: float = 10.0;
-
-private var nextFire: float = 0.5;
-private var newBullet: GameObject;
-private var myTime: float = 0.0;
-
-function start() {
-	// print("hello");
-}
+var bulletThrust: float = 10;
 
 function Update() {
 	handleMovement();
@@ -37,17 +28,10 @@ function handleMovement() {
 
 // Shoot primary weapon
 function handlePrimary() {
-	print("Primary weapon used");
-
-	nextFire = myTime + fireRate;
-
 	var position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
 	var direction = Camera.main.ScreenToWorldPoint(position);
 	bullet = Instantiate(bullet, transform.position, transform.rotation);   
 	bullet.GetComponent.<Rigidbody2D>().velocity = direction * bulletThrust;
-	
-	nextFire = nextFire - myTime;
-	myTime = 0.0;
 }
 
 function handleSecondary() {
