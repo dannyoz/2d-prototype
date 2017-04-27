6fcb9561c6c27020634a4234b750dad56d9a5bb5
Dan Osborne <dan.osborne@rocketmail.com>
2017-04-27 21:08:31 +0100

    Enemy shuffling added

--- Assets/Scripts/enemy.js
+++ Assets/Scripts/enemy.js
@@ -1,7 +1,24 @@
 ﻿#pragma strict
 
+var aPosition1 = new Vector2(3,3);
+var speed = 1;
+private var target : GameObject;
+
+function Update() {
+    shuffle();
+}
+
+function shuffle() {
+    target = GameObject.Find("Hero");
+    var from = new Vector2(transform.position.x, transform.position.y);
+    var to = new Vector2(target.transform.position.x, target.transform.position.y);
+    var time = speed * Time.deltaTime;
+    transform.position = Vector2.MoveTowards(from, to, time);
+}
+
 function reduceHealth(loss : float) {
     print(loss);
+    Destroy(this.gameObject);
 }
 
 function OnCollisionEnter2D (col : Collision2D) {
@@ -9,4 +26,3 @@ function OnCollisionEnter2D (col : Collision2D) {
         print(col.gameObject);
     }
 }
-
